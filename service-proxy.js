db67b34eb7a72e2d545ff559ef9019d3b8f4b95c
jleung51 <jleung513@gmail.com>
2016-09-25 01:35:48 -0700

    Configure server to send proxy request to Travis
    Right now, the request is not properly redirected.

--- service-proxy.js
+++ service-proxy.js
@@ -9,13 +9,43 @@ var dispatcher = require('httpdispatcher');
 
 const PORT = 8080;
 
-function setHttp201(response) {
-    response.writeHead(201);
+function clone(object) {
+    return JSON.parse(JSON.stringify(object))
+}
+
+var travisHttpTarget = {
+    host: 'api.travis-ci.org',
+    path: '/repo/' + process.env.REPO_OWNER + '%2F' + process.env.REPO_NAME + '/requests',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Travis-API-Version': 3,
+        'Authorization': process.env.TOKEN
+    },
+    followAllRedirects: true
+};
+
+var travisHttpCallback = function(response) {
+    var responseString = '';
+    console.log('Response: ');
+    console.log('  HTTP ' + response.statusCode);
+    console.log('  Headers: ' + JSON.stringify(response.headers));
+    response.on('data', function(data) {
+        responseString += data;
+    });
+    response.on('error', function(error) {
+        console.log(error.status);
+    });
+    response.on('end', function() {
+        console.log('  Body: ' + responseString);
+        console.log();
+    });
 }
 
 function handleReq(request, response) {
     try {
         console.log('Mapped ' + request.url);
+        console.log();
         dispatcher.dispatch(request, response);
     }
     catch(except) {
@@ -28,8 +58,17 @@ function setMappings(dispatcher) {
     dispatcher.setStatic('resources');
 
     dispatcher.onPost("/pacmacro/apitests", function(request, response) {
-       setHttp201(response);
-       response.end();
+        console.log('Sending HTTP request:');
+        console.log('  ' + travisHttpTarget.method + " " + travisHttpTarget.host + travisHttpTarget.path);
+        console.log('  Headers: ' + JSON.stringify(travisHttpTarget.headers));
+        console.log();
+
+        http.request(travisHttpTarget, travisHttpCallback).end();
+
+        response.writeHead(201);
+        console.log('Returning request with HTTP status code ' + response.statusCode);
+        console.log();
+        response.end();
     });
 
 }
@@ -38,5 +77,6 @@ var server = http.createServer(handleReq);
 setMappings(dispatcher);
 
 server.listen(PORT, function() {
-    console.log('Server running on http://localhost:%s', PORT);
+    console.log('Service Proxy is currently running on http://localhost:%s.', PORT);
+    console.log();
 });
